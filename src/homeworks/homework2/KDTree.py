1e739eac6d73d00dfd85c6ecf1d14a906c5b6d13
xImoZA <alekcandra.ri@gmail.com>
2025-03-03 20:39:42 +0300

    feat: add Point

--- src/homeworks/homework2/KDTree.py
+++ src/homeworks/homework2/KDTree.py
@@ -8,12 +8,31 @@ T = TypeVar("T", bound=float)
 
 
 @dataclass
+class Point(Generic[T]):
+    coord: tuple[T, ...]
+    cls: Optional[int]
+
+    def __lt__(self, other):
+        if isinstance(other, Point):
+            return self.coord < other.coord
+        return NotImplemented
+
+    def __eq__(self, other):
+        if isinstance(other, Point):
+            return self.coord == other.coord
+        return NotImplemented
+
+    def __hash__(self):
+        return hash(self.coord)
+
+
+@dataclass
 class Node(Generic[T]):
     axis: Optional[int] = None
     median: Optional[T] = None
     left: Optional["Node"] = None
     right: Optional["Node"] = None
-    points: Optional[list[tuple[T, ...]]] = None
+    points: list[Point[T]] | None = None
     is_leaf: bool = field(init=False)
 
     def __post_init__(self):
@@ -21,7 +40,7 @@ class Node(Generic[T]):
 
 
 class KDTree:
-    def __init__(self, points: list[tuple[T, ...]], leaf_size: int = 1):
+    def __init__(self, points: list[Point[T]], leaf_size: int = 1):
         if leaf_size <= 0:
             raise ValueError("leaf_size must by greater then 0")
         if len(points) == 0:
@@ -31,52 +50,52 @@ class KDTree:
         self.root: Node[T] = self._build_kdtree(points)
 
     @staticmethod
-    def _select_axis(points: list[tuple[T, ...]]) -> int:
-        variances = np.var(points, axis=0)
+    def _select_axis(points: list[Point[T]]) -> int:
+        variances = np.var([p.coord for p in points], axis=0)
         return int(np.argmax(variances))
 
-    def _build_kdtree(self, points: list[tuple[T, ...]]) -> Node[T]:
-        if len(points) <= self.leaf_size:
-            return Node(points=points)
+    def _build_kdtree(self, train: list[Point[T]]) -> Node[T]:
+        if len(train) <= self.leaf_size:
+            return Node(points=train)
 
-        axis = KDTree._select_axis(points)
+        axis = KDTree._select_axis(train)
 
-        points.sort(key=lambda point: point[axis])
-        median = len(points) // 2
+        train.sort(key=lambda point: point.coord[axis])
+        median = len(train) // 2
 
         return Node(
             axis=axis,
-            median=points[median][axis],
-            left=self._build_kdtree(points[:median]),
-            right=self._build_kdtree(points[median:]),
+            median=train[median].coord[axis],
+            left=self._build_kdtree(train[:median]),
+            right=self._build_kdtree(train[median:]),
         )
 
-    def query(
-        self, points: list[tuple[T]], k: int
-    ) -> dict[tuple[T], list[tuple[T, ...]]]:
+    def query(self, points: list[Point[T]], k: int) -> dict[Point[T], list[Point[T]]]:
         if len(points) == 0:
             raise ValueError("Points list must not be empty")
 
         if k <= 0:
             raise ValueError("k must be positive")
 
-        knn = {}
+        knn: dict[Point[T], list[Point[T]]] = {}
         for point in points:
-            knn_point = KDTree._search(point, k, self.root, [])
+            knn_point: list[tuple[float, Point[T]]] = KDTree._search(
+                point, k, self.root, []
+            )
             knn[point] = [heapq.heappop(knn_point)[1] for _ in range(k)]
         return knn
 
     @staticmethod
     def _search(
-        target: tuple[T],
+        target: Point[T],
         k: int,
-        node: Optional[Node[T]],
-        neighbors: list[tuple[float, tuple[T, ...]]],
-    ) -> list[tuple[float, tuple[T, ...]]]:
+        node: Node[T] | None,
+        neighbors: list[tuple[float, Point[T]]],
+    ) -> list[tuple[float, Point[T]]]:
         if node:
             if node.is_leaf and node.points:
                 for point in node.points:
-                    if len(point) != len(target):
+                    if len(point.coord) != len(target.coord):
                         raise ValueError("Points must have the same dimensionality")
 
                     dist: float = KDTree.distance(target, point)
@@ -94,7 +113,7 @@ class KDTree:
                 )
 
             axis = node.axis
-            if target[axis] < node.median:
+            if target.coord[axis] < node.median:
                 next_node = node.left
                 other_node = node.right
             else:
@@ -102,14 +121,17 @@ class KDTree:
                 other_node = node.left
 
             neighbors = KDTree._search(target, k, next_node, neighbors)
-            if len(neighbors) < k or abs(target[axis] - node.median) < -neighbors[0][0]:
+            if (
+                len(neighbors) < k
+                or abs(target.coord[axis] - node.median) < -neighbors[0][0]
+            ):
                 neighbors = KDTree._search(target, k, other_node, neighbors)
 
         return neighbors
 
     @staticmethod
-    def distance(point1: tuple[T, ...], point2: tuple[T, ...]) -> float:
-        if len(point1) != len(point2):
+    def distance(point1: Point[T], point2: Point[T]) -> float:
+        if len(point1.coord) != len(point2.coord):
             raise ValueError("Points must have the same dimensionality")
 
-        return np.sqrt(sum((x - y) ** 2 for x, y in zip(point1, point2)))
+        return np.sqrt(sum((x - y) ** 2 for x, y in zip(point1.coord, point2.coord)))
--- tests/homeworks/homework2/test_KDTree.py
+++ tests/homeworks/homework2/test_KDTree.py
@@ -1,15 +1,15 @@
 import math
+import random
 
 import hypothesis.strategies as st
-import numpy as np
 from hypothesis import given
 
-from src.homeworks.homework2.KDTree import KDTree, T
+from src.homeworks.homework2.KDTree import KDTree, Point, T
 
 
 def brute_force_search(
-    train: list[tuple[T]], test: list[tuple[T, ...]], k: int
-) -> dict[tuple[T, ...], list[tuple[T]]]:
+    train: list[Point[T]], test: list[Point[T]], k: int
+) -> dict[Point[T], list[Point[T]]]:
     knn = {}
     for test_point in test:
         neighbors = []
@@ -30,21 +30,27 @@ class TestKDTree:
         st.integers(min_value=1, max_value=10),
         st.integers(min_value=1, max_value=30),
     )
-    def test_query(self, size, k, leaf_size, near):
-        x_train = [tuple(row) for row in np.random.randint(-100, 100, size=(size, k))]
-        x_test = [tuple(row) for row in np.random.randint(-100, 100, size=(30, k))]
-
+    def test_query(self, train_size, k, leaf_size, neighbours):
+        x_train = [
+            Point(tuple(random.randint(-100, 100) for _ in range(k)), None)
+            for _ in range(train_size)
+        ]
+        x_test = [
+            Point(tuple(random.randint(-100, 100) for _ in range(k)), None)
+            for _ in range(30)
+        ]
         kdtree = KDTree(x_train, leaf_size)
-        tree_search = kdtree.query(x_test, near)
-        stupid_search = brute_force_search(x_train, x_test, near)
+
+        tree_search = kdtree.query(x_test, neighbours)
+        stupid_search = brute_force_search(x_train, x_test, neighbours)
 
         for point in x_test:
             stupid_dist = sorted(
-                KDTree.distance(point, near_point) for near_point in stupid_search
+                [KDTree.distance(point, near_point) for near_point in stupid_search]
             )
             tree_dist = sorted(
-                KDTree.distance(point, near_point) for near_point in tree_search
+                [KDTree.distance(point, near_point) for near_point in tree_search]
             )
 
-            for i in range(near):
+            for i in range(neighbours):
                 assert math.isclose(stupid_dist[i], tree_dist[i], rel_tol=1e-9)
